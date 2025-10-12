50bc7f6a8ddee1e00769a1f85a862c6b4d1f2246
Roman Novachok <romanihorovuch@gmail.com>
2025-10-12 20:02:32 +0300

    cummon update

--- db_lab_4-5/my_project/config.py
+++ db_lab_4-5/my_project/config.py
@@ -6,13 +6,17 @@ load_dotenv()
 def load_config():
     root_user = os.environ.get("MYSQL_ROOT_USER")
     root_password = os.environ.get("MYSQL_ROOT_PASSWORD")
+    db_host = os.environ.get("DB_HOST")
+    db_name = os.environ.get("DB_NAME")
+    jwt_secret = os.environ.get("JWT_SECRET")
 
     return {
         "SQLALCHEMY_DATABASE_URI": (
-            f"mysql+pymysql://{root_user}:{root_password}"
-            "@database-1.cpacg4020euz.eu-north-1.rds.amazonaws.com:3306/lab_4"
+            f"mysql+pymysql://{root_user}:{root_password}@{db_host}/{db_name}"
         ),
         "SECRET_KEY": os.environ.get("SECRET_KEY"),
+        "JWT_SECRET_KEY": jwt_secret,  # <-- ОСЬ ЦЕЙ ВАЖЛИВИЙ РЯДОК
         "DEBUG": os.environ.get("DEBUG", "False").lower() == "true",
         "SQLALCHEMY_TRACK_MODIFICATIONS": False
     }
+
