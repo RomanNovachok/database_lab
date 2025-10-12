c77476f7116b59faac614cc8f33e9b42642e0536
Roman Novachok <romanihorovuch@gmail.com>
2025-10-12 18:52:13 +0300

    cummon update

--- db_lab_4-5/my_project/auth/domain/__init__.py
+++ db_lab_4-5/my_project/auth/domain/__init__.py
@@ -6,19 +6,10 @@ apavelchak@gmail.com
 
 # Import here Domain Class that are needed for ORM
 # orders DB
+from my_project.auth.domain.orders.user import User  
 from my_project.auth.domain.orders.owner import Owner
 from my_project.auth.domain.orders.properties import Property
 from my_project.auth.domain.orders.amenity import Amenity
 from my_project.auth.domain.orders.property_amenity import PropertyAmenity
 from my_project.auth.domain.orders.review import Review
 from my_project.auth.domain.orders.owner_details import OwnerDetails
-from my_project.auth.domain.orders.user import User
-
-
-#
-# from my_project.auth.domain.orders.user import Users
-# from my_project.auth.domain.orders.bookings import Booking
-# from my_project.auth.domain.orders.authors import Author
-# from my_project.auth.domain.orders.client import Client
-# from my_project.auth.domain.orders.client_type import ClientType
-# from my_project.auth.domain.orders.books import Book
